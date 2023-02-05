735411c292deb41ab64a3c85c1ce6e6576cf238d
macs1756 <kindrativmaksum312@gmail.com>
2023-02-06 01:43:19 +0200

    screen rotate

--- js/module/resizeWindow.js
+++ js/module/resizeWindow.js
@@ -5,11 +5,15 @@ const resizeWindow = () =>{
 
 	function  resizeWindowItem(){
 
-	
+	console.log(window.innerHeight,window.innerWidth);
 	if(window.innerHeight > window.innerWidth){
+
 		document.querySelector(".game-off").classList.add('active');
+
 	}else{
+
 		document.querySelector(".game-off").classList.remove('active');
+
 	}
 }
 
